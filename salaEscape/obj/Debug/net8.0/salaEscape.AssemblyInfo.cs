ee6e2d167f62f61a361b3846bcc59532d83af82f
Manuel-Gartenkrot-Casal <manucasal07@gmail.com>
2025-06-01 18:35:47 -0300

    subiendo imagenes sala 1

--- salaEscape/obj/Debug/net8.0/salaEscape.AssemblyInfo.cs
+++ salaEscape/obj/Debug/net8.0/salaEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2a36544eb7e20b1e45b34b5fda6344eb2dacc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a9491f9a4074a354ca7d9de2e6441d18aeeaa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
