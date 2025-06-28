e9f12198b062e4433258001297d9adbf8f0e9e43
Tomas Goldentul <goldentultomas@gmail.com>
2025-06-28 13:38:10 -0300

    updated sala 4

--- salaEscape/obj/Debug/net8.0/salaEscape.AssemblyInfo.cs
+++ salaEscape/obj/Debug/net8.0/salaEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b37a0508cb1f6ec4db3a29a2a3968b1acf654b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347879773882c971b054a9d9b0c55831df8141f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("salaEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
